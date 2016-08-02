141423270a9b8a11a723d219d54b5897b9b2cd84
Uladzislau Harbachou <uladzislau_harbachou@epam.com>
2016-08-02 14:57:35 +0300

    update to webpack

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 var gulp = require('gulp');
+var jasmine = require('gulp-jasmine');
 
 gulp.task('spec', function () {
-    console.log("test");
+    return gulp.src('spec/test.js')
+        .pipe(jasmine({
+            config: "test/specs/_specs.json"
+        }))
 });
+
+
 
 gulp.task('default', ['spec']);
